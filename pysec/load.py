f4706dca3fe3c298bb4f85edcd77a6cd57a2054e
Federico Figus <figus.federico@gmail.com>
2014-03-19 20:49:17 +0100

    add docs import pysec.io.fd module add HEXDIGITS *constant* change a lot of internal names

--- pysec/load.py
+++ pysec/load.py
@@ -21,10 +21,12 @@ import os
 import hashlib
 import base64
 from types import ModuleType
+from pysec.io import fd
 
 
 ASCII_LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 DIGITS = '0123456789'
+HEXDIGITS = '0123456789abcdefABCDEF'
 
 
 _HASHES = {
@@ -35,14 +37,17 @@ _HASHES = {
 }
 
 
-_FIRST_LETTERS = '_%s' % string.ASCII_LETTERS
-_OTHER_LETTERS = '_%s%s' % (string.ASCII_LETTERS, string.DIGITS)
+_FIRST_LETTERS = '_%s' % ASCII_LETTERS
+_OTHER_LETTERS = '_%s%s' % (ASCII_LETTERS, DIGITS)
 
 
-is_hex = lambda s: all(ch in string.hexdigits for ch in s)
+def is_hex(string):
+    """Returns True if string is a valid hexadecimal number, otherwise False"""
+    return all(ch in HEXDIGITS for ch in string)
 
 
 def check_libname(name):
+    """Returns True if name is a valid string for a library, other False"""
     name = str(name)
     if not name:
         return 0
@@ -51,6 +56,8 @@ def check_libname(name):
 
 
 def parse_version(version):
+    """Parse versione string in a tuple, if version is an invalid string
+    returns None"""
     version = str(version).split('.')
     if len(version) != 3:
         return None
@@ -60,31 +67,34 @@ def parse_version(version):
 
 
 def parse_hashes(hashes):
+    """Parse hashes's string in hashes's dict, if it's invalid returns None"""
     _hashes = {}
     if hashes:
-        for hs in hashes.split(' '):
-            hname, _, hval = hs.partition(':')
-            hs = _HASHES.get(hname, None)
-            if hs is None:
+        for hs_field in hashes.split(' '):
+            hname, _, hval = hs_field.strip().partition(':')
+            hs_field = _HASHES.get(hname, None)
+            if hs_field is None:
                 return None
             if not is_hex(hval):
                 return None
-            if hs in _hashes:
+            if hs_field in _hashes:
                 return None
-            _hashes[hs] = hval
+            _hashes[hs_field] = hval
     return _hashes
 
 
-def _hash(path, hs):
-    with open(path, 'rb') as fp:
-        chunk = fp.read(4096)
+def _hash(path, hs_obj):
+    """Calculate the hash of path using hs_obj (a Hash Object)"""
+    with open(path, 'rb') as fmod:
+        chunk = fmod.read(4096)
         while chunk:
-            hs.update(chunk)
-            chunk = fp.read(4096)
+            hs_obj.update(chunk)
+            chunk = fmod.read(4096)
 
 
-def get_hash(path, hs):
-    hs = hs()
+def get_hash(path, hs_maker):
+    """Calculates the hash of module in path"""
+    hs_mod = hs_maker()
     if os.path.isfile(path):
         files = [path]
     elif os.path.isdir(path):
@@ -95,17 +105,18 @@ def get_hash(path, hs):
     else:
         raise ImportError("invalid file type %r" % path)
     for fpath in files:
-        _hash(fpath, hs)
-    return hs.hexdigest()
+        _hash(fpath, hs_mod)
+    return hs_mod.hexdigest()
 
 
 _CACHE = {}
 _TAB = {}
 
 
-class __LazyModule(ModuleType):
+class _LazyModule(ModuleType):
+    """_LazyModule is a module that instances their attribute in lazy mode"""
 
-    def __init___(self, name, version):
+    def __init__(self, name, version):
         self.name = str(name)
         self.version = version
         self.module = None
@@ -122,6 +133,7 @@ class __LazyModule(ModuleType):
 
 
 def load_tab(path):
+    """Updates internal tab of modules"""
     path = os.path.abspath(str(path))
     _tab = {}
     with fd.File.open(path, fd.FO_READ) as ftab:
@@ -180,15 +192,16 @@ def importlib(name, version=None, lazy=0, reload=0):
         imp.acquire_lock()
         path = mod_info['path']
         if lazy:
-            return __LazyModule(name, path)
+            return _LazyModule(name, path)
         else:
             fdir, fname = os.path.split(path)
-            for hs, hval in mod_info['hash'].iteritems():
-                if get_hash(path, hs) != hval:
+            for hs_maker, hval in mod_info['hash'].iteritems():
+                if get_hash(path, hs_maker) != hval:
                     raise ImportError("module %r %r in %r don't match hash %r"
                                       % (name, version, path, hval))
-            desc = imp.find_module(os.path.splitext(fname)[0], [fdir])
-            mod = imp.load_module(name, *desc)
+            fobj, path, desc = imp.find_module(os.path.splitext(fname)[0],
+                                               [fdir])
+            mod = imp.load_module(name, fobj, path, desc)
             _CACHE[(name, version)] = mod
             return mod
     finally:
