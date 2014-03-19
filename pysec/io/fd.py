a9e96a917e493f4ae968708f24bfc14770848368
Federico Figus <figus.federico@gmail.com>
2014-03-19 19:44:21 +0100

    add license in each file

--- pysec/io/fd.py
+++ pysec/io/fd.py
@@ -1,5 +1,21 @@
-"""Contains FD and FD-like classes for operation with file
-descriptor"""
+# Python Security Project (PySec) and its related class files.
+#
+# PySec is a set of tools for secure application development under Linux
+#
+# Copyright 2014 PySec development team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Contains FD and FD-like classes for operation with file descriptor"""
 from ..core import unistd
 from ..xsplit import xlines
 from ..alg import KNP_find
