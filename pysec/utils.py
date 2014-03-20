5fb79c28260643d0766f27f4e04ac73995e3c51d
Federico Figus <figus.federico@gmail.com>
2014-03-20 23:19:44 +0100

    add docs and change short names

--- pysec/utils.py
+++ pysec/utils.py
@@ -15,6 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""A lot of utilies for:
+    - Operations on paths
+    - Counting
+"""
 import os
 from itertools import izip_longest, islice
 import operator
@@ -22,10 +26,12 @@ import heapq
 
 
 def absjoinpath(*parts):
+    """Joins all path's parts and returns the absolute path"""
     return os.path.abspath(os.path.join(*parts))
 
 
 def path_split(path):
+    """Returns all the directories that constitute the full path""" 
     parts = []
     pre, post = os.path.split(os.path.normpath(str(path)))
     if not pre:
@@ -44,39 +50,42 @@ def is_subpath(path1, path2):
         1   if path2 is equal to path1
         2   if path2 is in directory path1
     """
-    for p1, p2 in izip_longest(path_split(str(path1)),
+    for part1, part2 in izip_longest(path_split(str(path1)),
                                path_split(str(path2)), fillvalue=None):
-        if p1 is None and p2 is None:
+        if part1 is None and part2 is None:
             return 1
-        elif p1 is None:
+        elif part1 is None:
             return 2
-        elif p2 is None:
+        elif part2 is None:
             return 0
-        if p1 != p2:
+        if part1 != part2:
             return 0
     return 1
 
 
 def subtract_path(prefix, path):
+    """Remove prefix path from path"""
     pre = []
     post = []
-    zl = izip_longest(path_split(str(prefix)), path_split(str(path)),
+    couples = izip_longest(path_split(str(prefix)), path_split(str(path)),
                       fillvalue=None)
-    for p1, p2 in zl:
-        if p1 is None and p2 is None:
+    for part1, part2 in couples:
+        if part1 is None and part2 is None:
             break
-        elif p1 == p2:
-            pre.append(p1)
+        elif part1 == part2:
+            pre.append(part1)
         else:
-            post.append(p2)
+            post.append(part2)
             break
-    for _, p2 in zl:
-        post.append(p2)
+    for _, part2 in couples:
+        post.append(part2)
     return os.path.join(*pre), os.path.join(*post)
 
 
 def xrange(start, stop, step=1):
-    n = int(start)
+    """This xrange use python's integers and have not limits of
+    machine integers."""
+    act = int(start)
     stop = int(stop)
     step = int(step)
     if step < 0:
@@ -86,15 +95,16 @@ def xrange(start, stop, step=1):
         bcmp = operator.lt
     else:
         raise StopIteration
-    while bcmp(n, stop):
-        yield n
-        n += step
+    while bcmp(act, stop):
+        yield act
+        act += step
 
 
-def top_n(values, n=10):
+def top_n(values, first_n=10):
+    """Returns the *n* greatest objects in values"""
     values = iter(values)
-    top = [val for val in islice(values, n)]
-    if len(top) < n:
+    top = [val for val in islice(values, first_n)]
+    if len(top) < first_n:
         return top
     heapq.heapify(top)
     for val in values:
