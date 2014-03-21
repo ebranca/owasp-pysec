6b99dac637d77b23ee3c08ca64e7506850440483
Federico Figus <figus.federico@gmail.com>
2014-03-21 22:52:16 +0100

    replace relative imports, add docs

--- pysec/io/fd.py
+++ pysec/io/fd.py
@@ -17,11 +17,11 @@
 # limitations under the License.
 #
 # -*- coding: ascii -*-
-"""Contains FD and FD-like classes for operation with file descriptor"""
-from ..core import unistd
-from ..xsplit import xlines
-from ..alg import knp_find
-from ..utils import xrange
+"""Contains FD and FD-like classes for operations with file descriptors"""
+from pysec.core import unistd
+from pysec.xsplit import xlines
+from pysec.alg import knp_find
+from pysec.utils import xrange
 import os
 import fcntl
 
@@ -54,8 +54,9 @@ class IncompleteWrite(Error):
 
 
 def read_check(func):
-    """Control for read methods"""
+    """Decorator to control read permission in reader methods"""
     def _read(fd, *args, **kargs):
+        """*func* wrapped with read check"""
         if not fd.flags & os.O_WRONLY:
             return func(fd, *args, **kargs)
         raise NotReadableFD(fd)
@@ -63,8 +64,9 @@ def read_check(func):
 
 
 def write_check(func):
-    """Control for write methods"""
+    """Decorator to control write permission in writer methods"""
     def _write(fd, *args, **kargs):
+        """*func* wrapped with write check"""
         if fd.flags & os.O_WRONLY:
             return func(fd, *args, **kargs)
         raise NotWriteableFD(fd)
@@ -72,6 +74,7 @@ def write_check(func):
 
 
 class FD(object):
+    """FD represents a File Descriptor"""
 
     def __init__(self, fd):
         fd = int(fd)
@@ -80,9 +83,11 @@ class FD(object):
         self.fd = fd
 
     def fileno(self):
+        """Return file descriptor's int"""
         return int(self.fd)
 
     def __int__(self):
+        """Return file descriptor's int"""
         return int(self.fd)
 
     def __enter__(self):
@@ -93,10 +98,12 @@ class FD(object):
         return 0
 
     def close(self):
+        """Closes file descriptor"""
         unistd.close(self.fd)
 
     # stat methods
     def stat(self):
+        """Returns result of a stat call"""
         return os.fstat(self.fd)
 
     @property
@@ -156,10 +163,12 @@ class FD(object):
     # fcntl methods
     @property
     def flags(self):
+        """Get file descriptor's flags (fcntl.F_GETFL)"""
         return int(fcntl.fcntl(self.fd, fcntl.F_GETFL))
 
     @flags.setter
     def flags(self, flags):
+        """Set file descriptor's flags (fcntl.F_SETFL)"""
         fcntl.fcntl(self.fd, fcntl.F_SETFL, int(flags))
 
 
@@ -179,35 +188,37 @@ FO_MODES = FO_READ, FO_WRNEW, FO_WREX, FO_APNEW, FO_APEX
 
 
 def _fo_read(fpath, mode):
-    return os.open(fpath, os.O_RDONLY)
+    return os.open(fpath, os.O_RDONLY, mode)
 
 
 def _fo_wrnew(fpath, mode):
-    return os.open(fpath, os.O_WRONLY | os.O_CREAT | os.O_EXCL)
+    return os.open(fpath, os.O_WRONLY | os.O_CREAT | os.O_EXCL, mode)
 
 
 def _fo_wrex(fpath, mode):
-    return os.open(fpath, os.O_WRONLY)
+    return os.open(fpath, os.O_WRONLY, mode)
 
 
 def _fo_apnew(fpath, mode):
-    return os.open(fpath, os.O_WRONLY | os.O_APPEND | os.O_CREAT | os.O_EXCL)
+    return os.open(fpath, os.O_WRONLY | os.O_APPEND | os.O_CREAT | os.O_EXCL, mode)
 
 
 def _fo_apex(fpath, mode):
-    return os.open(fpath, os.O_WRONLY | os.O_APPEND)
+    return os.open(fpath, os.O_WRONLY | os.O_APPEND, mode)
 
 
 _FOMODE2FUNC = _fo_read, _fo_wrnew, _fo_wrex, _fo_apnew, _fo_apex
 
 
 class File(FD):
+    """File represents a Regular File's file descriptor."""
 
     def __init__(self, fd):
         super(self.__class__, self).__init__(fd)
         self.pos = 0
 
     def __len__(self):
+        """Returns file's size"""
         return os.fstat(self.fd).st_size
 
     def __getitem__(self, index):
@@ -224,6 +235,8 @@ class File(FD):
 
     @staticmethod
     def open(fpath, oflag, mode=0666):
+        """Open a file descript for a regular file in fpath using the open mode
+        specifie by *oflag* with *mode*"""
         oflag = int(oflag)
         mode = int(mode)
         if oflag not in FO_MODES:
@@ -234,6 +247,8 @@ class File(FD):
 
     @staticmethod
     def touch(fpath, mode=0666):
+        """Create a new file with passed *mode* in *fpath*.
+        If file *fpath* exists, a IOError will be raised."""
         fd = -1
         try:
             fd = os.open(fpath, os.O_RDONLY | os.O_CREAT, mode)
@@ -243,6 +258,7 @@ class File(FD):
 
     @read_check
     def read(self, size=None):
+        """Read *pos*-length data starting from position *pos*."""
         size = int(self.size) if size is None else int(size)
         pos = int(self.pos)
         if size < 0:
@@ -253,6 +269,8 @@ class File(FD):
 
     @read_check
     def pread(self, size, pos):
+        """Read *pos*-length data starting from position *pos*.
+        This operation doesn't change the pointer position."""
         size = int(size)
         pos = int(pos)
         if size < 0:
@@ -262,6 +280,10 @@ class File(FD):
 
     @write_check
     def write(self, data, tries=3):
+        """Write data starting from position *pos* and do maximum *tries*
+        write attempt, if all will fail it raises a IncompleteWrite
+        exception. This operation moves the position pointer at end of written
+        data."""
         fd = int(self)
         _tries = tries = int(tries)
         pos = int(self.pos)
@@ -283,6 +305,9 @@ class File(FD):
 
     @write_check
     def pwrite(self, data, pos, tries=3):
+        """Write data starting from position *pos* and do maximum *tries*
+        write attempt, if all will fail it raises a IncompleteWrite
+        exception. This operation doesn't change the pointer position."""
         fd = int(self)
         _tries = tries = int(tries)
         pos = int(pos)
@@ -302,13 +327,21 @@ class File(FD):
                 _tries = tries
 
     def moveto(self, pos):
+        """Move position pointer in position *pos* from start of FD."""
         self.pos = int(pos)
 
     def lines(self, start=None, stop=None, eol='\n', keep_eol=1):
+        """Splits FD's content in lines that end with *eol*, it'll start from
+        *start* position and it'll stop at stop position, if *stop* is None it
+        will stop at the end of FD. If keep_eol is true doesn't remove *eol*
+        from the line"""
         start = self.pos if start is None else int(start)
         return xlines(self, eol, keep_eol, start, stop, knp_find)
 
     def chunks(self, size,  start=0, stop=None):
+        """Divides FD's content in chunk of length *size* starting from *start*
+        and stopping at *stop*, if *stop* is None it'll stop at end of FD's
+        content."""
         size = int(size)
         for offset in xrange(*slice(int(start),
                              None if stop is None
@@ -317,20 +350,25 @@ class File(FD):
 
 
 class Directory(FD):
+    """File represents a Regular File's file descriptor."""
     pass
 
 
 class Socket(FD):
+    """File represents a Socket's file descriptor."""
     pass
 
 
 class BlockDev(FD):
+    """File represents a Block Device's file descriptor."""
     pass
 
 
 class CharDev(FD):
+    """File represents a Character Device's file descriptor."""
     pass
 
 
 class FIFO(FD):
+    """File represents a FIFO's file descriptor."""
     pass
