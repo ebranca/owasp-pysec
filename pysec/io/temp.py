44e142e5931db6a758dc5ceb70556f47a97e09ff
Federico Figus <figus.federico@gmail.com>
2014-03-22 22:29:22 +0100

    fix issue #7

--- pysec/io/temp.py
+++ pysec/io/temp.py
@@ -23,9 +23,19 @@ import errno
 import atexit
 from tempfile import _get_candidate_names, _set_cloexec, TMP_MAX
 from pysec.io import fd
+from pysec.io import fcheck
 
 
-def mkstemp(dirpath, prefix, suffix, unlink=1):
+
+
+def mkshide(dirpath):
+    """Create a file without path"""
+    raise NotImplementedError
+    # open with O_TMPFILE flag in linux 3.11
+    # or open and unlink immediately
+
+
+def mkstemp(dirpath, prefix='', suffix='', unlink=1, mode=0600):
     """Creates a file in directory *dir* using *prefix* and *suffix* to
     name it:
             (dir)/<prefix><random_string><postfix>
@@ -34,6 +44,9 @@ def mkstemp(dirpath, prefix, suffix, unlink=1):
     If *unlink* is true registers a unlink function at exit.
     """
     dirpath = os.path.abspath(dirpath)
+    mode = int(mode)
+    if not fcheck.mode_check(mode):
+        raise ValueError("wrong mode: %r" % oct(mode))
     names = _get_candidate_names()
     for _ in xrange(TMP_MAX):
         name = names.next()
@@ -42,8 +55,8 @@ def mkstemp(dirpath, prefix, suffix, unlink=1):
             atexit.register(os.unlink, fpath)
         fdr = fdw = -1
         try:
-            fdr = os.open(fpath, os.O_RDONLY | os.O_CREAT | os.O_EXCL, 0600)
-            fdw = os.open(fpath, os.O_WRONLY, 0600)
+            fdr = os.open(fpath, os.O_RDONLY | os.O_CREAT | os.O_EXCL, mode)
+            fdw = os.open(fpath, os.O_WRONLY, mode)
             _set_cloexec(fdr)
             _set_cloexec(fdw)
             return fd.File(fdr), fd.File(fdw)
@@ -73,7 +86,7 @@ def mkstemp(dirpath, prefix, suffix, unlink=1):
     raise IOError(errno.EEXIST, "No usable temporary file name found")
 
 
-def mkdtemp(dirpath, prefix, suffix):
+def mkdtemp(dirpath, prefix='', suffix='', mode=0700):
     """Creates a directory in directory *dir* using *prefix* and *suffix* to
     name it:
             (dir)/<prefix><random_string><postfix>
@@ -81,12 +94,15 @@ def mkdtemp(dirpath, prefix, suffix):
     """
     dirpath = os.path.abspath(dirpath)
     names = _get_candidate_names()
+    mode = int(mode)
+    if not fcheck.mode_check(mode):
+        raise ValueError("wrong mode: %r" % oct(mode))
     for _ in xrange(TMP_MAX):
         name = names.next()
         fpath = os.path.abspath(os.path.join(dirpath, '%s%s%s'
                                 % (prefix, name, suffix)))
         try:
-            os.mkdir(fpath, 0700)
+            os.mkdir(fpath, mode)
             return fpath
         except OSError, ex:
             if ex.errno == errno.EEXIST:
