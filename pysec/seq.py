48e5e447f2dd6bf543b4932b867d9664dcc21234
Phygus <figus.federico@gmail.com>
2014-05-19 11:33:59 +0200

    add seq module

---
+++ pysec/seq.py
@@ -0,0 +1,14 @@
+"""Various utilities fot sequences manipulation"""
+from pysec.utils import xrange
+
+
+def ioc(seq, shift=1):
+    """Return the index of coincidence"""
+    match = 0
+    seq_len = len(seq)
+    for i in xrange(0, seq_len):
+        j = (i + shift) % seq_len
+        if seq[i] == seq[j]:
+            match += 1
+    return float(match) / float(seq_len)
+
