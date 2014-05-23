30bdc03e60e2fa2332523e8ce5c1cf4ca7b693bd
Phygus <figus.federico@gmail.com>
2014-05-23 12:23:57 +0200

    add centroid, next_multiple, div_ceil

--- pysec/maths.py
+++ pysec/maths.py
@@ -18,6 +18,7 @@
 #
 # -*- coding: ascii -*-
 """Various math utilities"""
+import stats
 
 
 def distance(p1, p2):
@@ -27,4 +28,29 @@ def distance(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def centroid(*points):
+    """Calculate the centroid point of a points set in a 2-dimensional space"""
+    cx = stats.avg()
+    cy = stats.avg()
+    cx.next()
+    cy.next()
+    for x, y in points:
+        x = float(x)
+        y = float(y)
+        cx.send(x)
+        cy.send(y)
+    return cx.next(), cy.next()
+
+
+def div_ceil(n, m):
+    """Return the smallest integer x such that x * m >= n."""
+    q, r = divmod(n, m)
+    return q + int(r != 0)
+
+
+def next_multiple(n, m):
+    """Return the smallest multiple of m greater than n"""
+    return div_ceil(n, m) * m
 
