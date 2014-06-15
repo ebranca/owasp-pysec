3e096807e7fe2f80ce64fbaeaf91c036ab0d5221
Federico Figus <federico.figus@owasp.org>
2014-06-15 18:39:35 +0200

    add traceback formatters

--- pysec/tb.py
+++ pysec/tb.py
@@ -18,11 +18,15 @@
 #
 # -*- coding: ascii -*-
 """Traceback utilities."""
+import dis
 import keyword
-import tokenize
+import opcode
+import os
 import sys
+import tokenize
 
 from pysec.core import Object
+from pysec.io import fd
 from pysec.xsplit import xlines
 
 
@@ -106,11 +110,127 @@ class Hook(Object):
         self.handle((exc_type, exc_val, exc_tb))
 
     def handle(self, info=None):
-        self.out.write(self.formatter(info or sys.exc_info()))
-        self.out.write('\n')
+        self.out.write(self.formatter(*(info or sys.exc_info())))
 
 
+def set_excepthook(fmt, out=sys.stderr):
+    sys.excepthook = Hook(fmt, out)
 
-def reset_exceptook():
+
+def reset_excepthook():
     sys.excepthook = sys.__excepthook__
+
+
+# Utilities to format traceback
+
+# inspired by dis.disassemble() in Python 2.7
+def disassemble(co):
+    code = co.co_code
+    labels = dis.findlabels(code)
+    linestarts = dict(dis.findlinestarts(co))
+    n = len(code)
+    i = 0
+    extended_arg = 0
+    free = None
+    lineno = None
+    while i < n:
+        c = code[i]
+        op = ord(c)
+        lineno = linestarts.get(i, lineno)
+        is_label = i in labels
+        ist = i
+        i += 1
+        if op >= opcode.HAVE_ARGUMENT:
+            oparg = ord(code[i]) + ord(code[i + 1]) * 256 + extended_arg
+            extended_arg = 0
+            i += 2
+            if op == opcode.EXTENDED_ARG:
+                extended_arg = oparg * 65536L
+            if op in opcode.hasconst:
+                arg = co.co_consts[oparg]
+            elif op in opcode.hasname:
+                arg = co.co_names[oparg]
+            elif op in opcode.hasjrel:
+                arg = i + oparg
+            elif op in opcode.haslocal:
+                arg = co.co_varnames[oparg]
+            elif op in opcode.hascompare:
+                arg = opcode.cmp_op[oparg]
+            elif op in opcode.hasfree:
+                if free is None:
+                    free = co.co_cellvars + co.co_freevars
+                arg = free[oparg]
+            else:
+                arg = NOVAL
+        else:
+            arg = NOVAL
+        yield ist, lineno, is_label, opcode.opname[op], arg
+
+
+def short_tb(exc_type, exc_value, exc_tb):
+    traceback = []
+    while exc_tb:
+        traceback.append('{%r, %r, %r}' % (exc_tb.tb_frame.f_code.co_filename,
+                                           exc_tb.tb_frame.f_code.co_name,
+                                           exc_tb.tb_lineno))
+        exc_tb = exc_tb.tb_next
+    return 'Traceback: %s\nError: %s %r\n' % (' -> '.join(traceback), exc_type.__name__, str(exc_value))
+
+
+def long_tb(exc_type, exc_value, exc_tb):
+    traceback = ['Traceback (most recent call last):']
+    lvl = 0
+    while exc_tb:
+        path = os.path.abspath(exc_tb.tb_frame.f_code.co_filename)
+        lineno = exc_tb.tb_lineno - 1
+        traceback.append('[%d]' % lvl)
+        traceback.append('  Where: %r:%d %r' % (path, lineno, exc_tb.tb_frame.f_code.co_name))
+        with fd.File.open(path, fd.FO_READEX) as src:
+            line = src.get_line(lineno)
+        traceback.append('  Line: %r' % line.strip())
+        traceback.append('  Variables:')
+        for token, where, val in linevars(line.strip(), exc_tb.tb_frame):
+            if where is None:
+                val = '<undefined>'
+            traceback.append('    %r: %r' % (token, val))
+        exc_tb = exc_tb.tb_next
+        lvl += 1
+    return '%s\n[ERROR]\n%s: %r\n' % ('\n'.join(traceback), exc_type.__name__, str(exc_value))
+
+
+def deep_tb(exc_type, exc_value, exc_tb):
+    traceback = ['=== Traceback (most recent call last) ===']
+    lvl = 0
+    prev = None
+    while exc_tb:
+        path = os.path.abspath(exc_tb.tb_frame.f_code.co_filename)
+        lineno = exc_tb.tb_lineno
+        traceback.append('[%d]' % lvl)
+        traceback.append('  File: %r' % path)
+        traceback.append('  Function: %r' % exc_tb.tb_frame.f_code.co_name)
+        with fd.File.open(path, fd.FO_READEX) as src:
+            line = src.get_line(lineno - 1)
+        traceback.append('  Line: (%d) %r' % (lineno, line.strip()))
+        traceback.append('  Variables:')
+        for token, where, val in linevars(line.strip(), exc_tb.tb_frame):
+            if where is None:
+                val = '<undefined>'
+            traceback.append('    %r: %r' % (token, val))
+        prev = exc_tb
+        exc_tb = exc_tb.tb_next
+        lvl += 1
+    code = prev.tb_frame.f_code
+    traceback.append('Code:')
+    for ist, lineno, label, op, arg in disassemble(prev.tb_frame.f_code):
+        prefix = '>> ' if ist == prev.tb_lasti else '   '
+        postfix = ' << %s' % exc_type.__name__ if ist == prev.tb_lasti else ''
+        if lineno == prev.tb_lineno:
+            if arg is NOVAL:
+                traceback.append('  %s%s%s' % (prefix, op, postfix))
+            else:
+                traceback.append('  %s%s %r%s' % (prefix, op, arg, postfix))
+    traceback.append('[ERROR]')
+    traceback.append('%s: %r' % (exc_type.__name__, str(exc_value)))
+    traceback.append('=========================================\n')
+    return '\n'.join(traceback)
 
