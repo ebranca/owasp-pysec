e853e6301f86e33a96d46cdf825a9b87f0b0ddf0
Federico Figus <federico@volcano>
2014-11-22 15:48:10 +0100

    initial commit

--- demo/import/paypal/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-from paypal.interface import PayPalInterface
-from paypal.settings import PayPalConfig
-from paypal.exceptions import PayPalError, PayPalConfigError, PayPalAPIResponseError
-import paypal.countries
-
-VERSION = '1.0.3'
--- tests/io_tests/test_runall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple script to load python files from a folder and execute them
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-for test_py in `(ls $DIR/*.py)`
-do
-    python $test_py
-done
